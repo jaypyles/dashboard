5ec42009a40150be4e798df68c26fdf007612cb9
Jayden Pyles <jpylesbusiness@gmail.com>
2024-12-15 16:27:38 -0600

    fix: hosts not loading

--- api/backend/host_manager.py
+++ api/backend/host_manager.py
@@ -1,8 +1,20 @@
 # LOCAL
 from api.backend.systems.host import Host
 import os
+import logging
 
-config_files = [f"./configs/{config}" for config in os.listdir("./configs") 
-                if os.path.isfile(f"./configs/{config}")]
+LOG = logging.getLogger(__name__)
+
+
+config_files = [
+    f"./configs/{config}"
+    for config in os.listdir("./configs")
+    if os.path.isfile(f"./configs/{config}")
+]
+
+LOG.info(f"Loaded config files: {config_files}")
+
 hosts = [Host(f) for f in config_files]
 HOST_MAP = {host.name: host for host in hosts}
+
+LOG.info(f"Loaded hosts: {HOST_MAP}")
