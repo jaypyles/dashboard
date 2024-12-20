51134fa92a6a5974dda8fe97afc76e57c8a1c722
Jayden Pyles <jpylesbusiness@gmail.com>
2024-12-20 15:24:06 -0600

    fix: use 127.0.0.1

--- src/lib/services/api/api.ts
+++ src/lib/services/api/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const cacheApi = setupCache(
   axios.create({
-    baseURL: `http://localhost:8000/api`,
+    baseURL: `http://127.0.0.1:8000/api`,
     timeout: 20000,
     headers: {
       "Content-Type": "application/json",
@@ -15,7 +15,7 @@ const cacheApi = setupCache(
 );
 
 const api = axios.create({
-  baseURL: `http://localhost:8000/api`,
+  baseURL: `http://127.0.0.1:8000/api`,
   timeout: 20000,
   headers: {
     "Content-Type": "application/json",
